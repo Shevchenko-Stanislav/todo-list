f4ccfd57533e996dfe9f92ce5baf75812f0a218a
Stanislav <sa_shev@mail.ru>
2022-09-06 22:51:03 +0300

    h fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
         <div className="min-h-screen py-10 max-w-3xl m-auto">
             <h1 className="text-3xl font-bold mb-5 text-center">Todo List</h1>
             <ThemeToggleButton />
-            <div className="max-w-4xl px-3" style={{height: "70vh", overflowY: "scroll"}}>
+            <div className="max-w-4xl px-3" style={{height: "60vh", overflowY: "scroll"}}>
                 <TodoList/>
             </div>
             <InputTask/>
